4e96d459d4fc7296e2aba1c91e19fab4be8a2d30
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2025-01-26 13:40:18 +0530

    Update reload.py

--- AlexaMusic/plugins/tools/reload.py
+++ AlexaMusic/plugins/tools/reload.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+# Copyright (C) 2025 by NOBITA_SUPPORT @ Github, < https://github.com/vishalpandeynkp1 >
+# Subscribe On YT < VISHAL PANDEY TEAM >. All rights reserved. © Alexa © Yukki.
 
 """"
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
+NOBITA_SUPPORT is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Nobita <https://github.com/vishalpandeynkp1>
 
 This program is free software: you can redistribute it and can modify
 as you want or you can collabe if you have new ideas.
